6ceb8686ece9efb984f2ebd27b3070adcfbe95ac
Darren Kirby <bulliver@gmail.com>
2014-09-15 11:23:27 -0600

    Added function prototypes. Added trim_whitespace(). Added #Include <string.h>

--- src/common.h
+++ src/common.h
@@ -27,6 +27,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
+#include <ctype.h>
+
 
 /* For longopts */
 #define _GNU_SOURCE
@@ -35,6 +38,13 @@
 /* Version information */
 #define APPSUITE "dgnu-utils"
 #define APPVERSION "0.2"
+
+/* prototypes */
+
+void g_error(char *message);
+int f_error(char *filename, char *message);
+int dump_args(int argc, char *argv[]);
+char *trim_whitespace(char *str);
 
 /* Generic error */
 void g_error(char *message) {
@@ -57,6 +67,47 @@ int dump_args(int argc, char *argv[]) {
         printf("argv[%i]: %s\n", i, argv[i]);
     }
     return 0;
+}
+
+
+/* trims leading and tailing whitspace from strings */
+char *trim_whitespace(char *str) {
+    size_t len = 0;
+    char *frontp = str;
+    char *endp = NULL;
+
+    if( str == NULL ) { return NULL; }
+    if( str[0] == '\0' ) { return str; }
+
+    len = strlen(str);
+    endp = str + len;
+
+    /* Move the front and back pointers to address the first non-whitespace
+     * characters from each end.
+     */
+    while( isspace(*frontp) ) { ++frontp; }
+    if( endp != frontp )
+    {
+        while( isspace(*(--endp)) && endp != frontp ) {}
+    }
+
+    if( str + len - 1 != endp )
+            *(endp + 1) = '\0';
+    else if( frontp != str &&  endp == frontp )
+            *str = '\0';
+
+    /* Shift the string so that it starts at str so that if it's dynamically
+     * allocated, we can still free it on the returned pointer.  Note the reuse
+     * of endp to mean the front of the string buffer now.
+     */
+    endp = str;
+    if( frontp != str )
+    {
+            while( *frontp ) { *endp++ = *frontp++; }
+            *endp = '\0';
+    }
+    
+    return str;
 }
 
 
