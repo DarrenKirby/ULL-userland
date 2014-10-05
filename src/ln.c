a45007c3196954831c732fdc40a72b222817904f
Darren Kirby <bulliver@gmail.com>
2014-10-04 22:01:26 -0600

    Hacked a bit

--- src/ln.c
+++ src/ln.c
@@ -24,14 +24,23 @@
 #include "common.h"
 
 void show_help(void) {
-    printf("Usage: %s [OPTION]...\n\n \
+    printf("Usage: %s [OPTION]... TARGET LINK_NAME\t(1st form)\n \
+  or: %s [OPTION]... TARGET\t\t(2nd form)\n\n \
+    -s, --symbolic\tmake symbolic links instead of hard links\n \
+    -f, --force\tdo not prompt before overwriting files\n \
+    -i, --interactive\tprompt before overwriting files\n \
+    -v, --verbose\tprint out links created\n \
     -h, --help\t\tdisplay this help\n \
     -V, --version\tdisplay version information\n\n \
-    Report bugs to <bulliver@gmail.com>\n", APPNAME);
+    Report bugs to <bulliver@gmail.com>\n", APPNAME, APPNAME);
 }
 
 int main(int argc, char *argv[]) {
     int opt;
+    int symbolic = 0;
+    int force = 0;
+    int verbose = 0;
+    int interactive = 0;
 
     struct option longopts[] = {
         {"symbolic", 0, NULL, 's'},
@@ -45,10 +54,82 @@ int main(int argc, char *argv[]) {
 
     while ((opt = getopt_long(argc, argv, "sfivVh", longopts, NULL)) != -1) {
         switch(opt) {
-            case 'V': printf("%s (%s) version %s\n", APPNAME, APPSUITE, APPVERSION); exit(EXIT_SUCCESS); break;
-            case 'h': show_help(); exit(EXIT_SUCCESS); break;
-            default : show_help(); exit(EXIT_FAILURE); break;
+            case 'V': 
+                printf("%s (%s) version %s\n", APPNAME, APPSUITE, APPVERSION);
+                printf("%s compiled on %s at %s\n", basename(__FILE__), __DATE__, __TIME__);
+                exit(EXIT_SUCCESS);
+                break;
+            case 'h': 
+                show_help(); 
+                exit(EXIT_SUCCESS); 
+                break;
+            case 's':
+                symbolic = 1;
+                break;
+            case 'f':
+                force = 1;
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            case 'i':
+                interactive = 1;
+                break;
+            case ':':
+                 /* getopt_long prints own error message */
+                exit(EXIT_FAILURE);
+                break;
+            case '?':
+                 /* getopt_long prints own error message */
+                exit(EXIT_FAILURE);
+            default : 
+                show_help(); 
+                exit(EXIT_FAILURE); 
+                break;
         }
     }
+    printf("Argc: %i\n", argc);
+    /*if (argc > 3) {
+        perror("too many arguments");
+        exit(EXIT_FAILURE);
+    }
+    */
+    char target[PATH_MAX];
+    char *target_p = target; 
+    strncpy(target_p, argv[argc], PATH_MAX);
+    char name[PATH_MAX];
+    char *name_p = name;
+    strncpy(name_p, argv[argc + 1], PATH_MAX);
+
+    if (argc) { /* 1st form */
+        if (access(argv[1], F_OK) == 0) {
+            if (force == 1) 
+                unlink(argv[1]);
+            if (interactive == 1) {
+                char response; 
+                printf("%s exits, overwrite? ('y' or 'n') ", argv[1]);
+                do {
+                response = getchar();
+                } while (response == '\n');
+
+                if (response == 'y' || response == 'Y')
+                    unlink(argv[1]);
+            }
+        }
+        if (symbolic == 1)
+            symlink(argv[1], argv[2]);
+        else
+            link(argv[1], argv[2]);
+        if (verbose == 1)
+            printf("linked %s to %s\n", argv[1], argv[2]);
+            
+
+    } else if (argc == 2) { /* 2nd form */
+        ;    
+    } else {
+        perror("not enough arguments");
+        exit(EXIT_FAILURE);
+    }
+        
     return EXIT_SUCCESS;
 }
