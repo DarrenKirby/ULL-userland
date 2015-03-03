02dc9dbd135f0ca5a9f037a1c09ff4a5dda2f0e3
Darren Kirby <bulliver@gmail.com>
2015-03-03 16:24:36 -0700

    Added 'od'

---
+++ src/od.c
@@ -0,0 +1,129 @@
+/***************************************************************************
+ *   od.c - object dump                                                    *
+ *                                                                         *
+ *   Copyright (C) 2014 by Darren Kirby                                    *
+ *   bulliver@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#define APPNAME "od"
+#include "common.h"
+
+void show_help(void) {
+    printf("Usage: %s [OPTION]...\n\n\
+Options:\n\
+    -h, --help\t\tdisplay this help\n\
+    -V, --version\tdisplay version information\n\n\
+Report bugs to <bulliver@gmail.com>\n", APPNAME);
+}
+
+int read_file_hex(char *filename[]) {
+    FILE *fp;
+    char c;
+
+    if ((fp = fopen(*filename, "r")) == NULL ) {
+        f_error(*filename, "Cannot open ");
+    }
+
+    int offset = 0;
+
+    printf("0x%08x ", offset);
+    while (( c = getc(fp)) != EOF ) {
+        printf("%02x", (unsigned)c );
+        offset++;
+
+        if (offset % 2 == 0) {
+            printf(" ");
+        }
+
+        if (offset % 16 == 0) {
+            printf("\n");
+            printf("0x%08x ", offset);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int read_file_oct(char *filename[]) {
+    FILE *fp;
+    char c;
+
+    if ((fp = fopen(*filename, "r")) == NULL ) {
+        f_error(*filename, "Cannot open ");
+    }
+
+    int offset = 0;
+
+    printf("0%08o ", offset);
+    while (( c = getc(fp)) != EOF ) {
+        printf("%03o", (unsigned)c );
+        offset++;
+
+        //if (offset % 2 == 0) {
+            printf(" ");
+        // }
+
+        if (offset % 16 == 0) {
+            printf("\n");
+            printf("0%08o ", offset);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    int opt;
+
+    struct option longopts[] = {
+        {"help", 0, NULL, 'h'},
+        {"version", 0, NULL, 'V'},
+        {0,0,0,0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "Vh", longopts, NULL)) != -1) {
+        switch(opt) {
+            case 'V':
+                printf("%s (%s) version %s\n", APPNAME, APPSUITE, APPVERSION);
+                printf("%s compiled on %s at %s\n", basename(__FILE__), __DATE__, __TIME__);
+                exit(EXIT_SUCCESS);
+                break;
+            case 'h':
+                show_help();
+                exit(EXIT_SUCCESS);
+                break;
+            case ':':
+                 /* getopt_long prints own error message */
+                exit(EXIT_FAILURE);
+                break;
+            case '?':
+                 /* getopt_long prints own error message */
+                exit(EXIT_FAILURE);
+            default:
+                show_help();
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+
+    read_file_oct(&argv[1]);
+
+    return EXIT_SUCCESS;
+}
