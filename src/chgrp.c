e60ea0f6c0374ae800ba68eff130415f71f768eb
Darren Kirby <bulliver@gmail.com>
2025-08-18 10:26:51 -0700

    Minor code cleanups

--- src/chgrp.c
+++ src/chgrp.c
@@ -34,17 +34,18 @@
 const char *APPNAME = "chgrp";
 
 struct group *grp_buf;
-char to_grp[FILEMAX+1];
+char to_grp[FILEMAX];
 
-struct optstruct {
-    int nodereference;
+struct opt_struct {
+    int no_dereference;
     int recursive;
     int verbose;
 } opts;
 
 
 static void show_help(void) {
-    printf("Usage: %s [OPTION] group file ...\n\n\
+    printf("Usage: %s [OPTION] group FILE [FILE]...\n\n\
+Change group ownership of file(s)\n\n\
 Options:\n\
     -R, --recursive\t\tchange group of files recursively\n\
     -v, --verbose\t\toutput a diagnostic for every file processed\n\
@@ -54,44 +55,42 @@ Options:\n\
 Report bugs to <bulliver@gmail.com>\n", APPNAME);
 }
 
-static int chgrp_recurse(const char *path, const struct stat *statptr, int type, struct FTW *pfwt) {
-
+int chgrp_recurse(const char *path, const struct stat *stat_ptr, const int type, struct FTW *p_fwt) {
     if (type == FTW_NS) {
-        printf("stat failed on `%s' (permissions?)\n", path);
+        fprintf(stderr, "stat failed on `%s' (permissions?)\n", path);
+        return EXIT_FAILURE;
         /* non-fatal */
-        return 0;
     }
 
-    if (type == FTW_SL && opts.nodereference == 1) {
+    if (type == FTW_SL && opts.no_dereference == 1) {
         if (lchown(path, -1, grp_buf->gr_gid) != 0) {
-                perror("chown failed");
-            }
+            fprintf(stderr, "lchown failed on `%s'\n", path);
+        }
     } else {
         if (chown(path, -1, grp_buf->gr_gid) != 0) {
-            perror("chown failed");
+            fprintf(stderr, "chown failed on `%s'\n", path);
         }
     }
 
     if (opts.verbose == 1) {
         printf("Changed group ownership of `%s' to `%s'\n", path, to_grp);
     }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-int main(int argc, char *argv[]) {
+int main(const int argc, char *argv[]) {
     int opt;
 
-    struct option longopts[] = {
+    const struct option long_opts[] = {
         {"help",           0, NULL, 'h'},
         {"version",        0, NULL, 'V'},
         {"recursive",      0, NULL, 'R'},
         {"verbose",        0, NULL, 'v'},
         {"no-dereference", 0, NULL, 'd'},
-        {0,0,0,0}
+        {NULL,0,NULL,0}
     };
 
-    while ((opt = getopt_long(argc, argv, "VhRvd", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "VhRvd", long_opts, NULL)) != -1) {
         switch(opt) {
             case 'V':
                 printf("%s (%s) version %s\n", APPNAME, APPSUITE, APPVERSION);
@@ -109,14 +108,8 @@ int main(int argc, char *argv[]) {
                 opts.verbose = 1;
                 break;
             case 'd':
-                opts.nodereference = 1;
+                opts.no_dereference = 1;
                 break;
-            case ':':
-                 /* getopt_long prints own error message */
-                exit(EXIT_FAILURE);
-            case '?':
-                 /* getopt_long prints own error message */
-                exit(EXIT_FAILURE);
             default:
                 show_help();
                 exit(EXIT_FAILURE);
@@ -134,37 +127,32 @@ int main(int argc, char *argv[]) {
     optind++;
 
     if (opts.recursive == 1) {
-
-        if (opts.nodereference == 1) {
-            if (nftw(argv[optind], chgrp_recurse, 10, FTW_PHYS) != 0) {
-                perror("chgrp");
-            }
+        /* Status of chgrp calls checked within chgrp_recursive()
+         * A failure of any particular file will be noted, but we
+         * will continue to run... */
+        if (opts.no_dereference == 1) {
+            nftw(argv[optind], chgrp_recurse, 10, FTW_PHYS);
         } else {
-            if (nftw(argv[optind], chgrp_recurse, 10, 0) != 0) {
-                perror("chgrp");
-            }
+            nftw(argv[optind], chgrp_recurse, 10, 0);
         }
         return EXIT_SUCCESS;
     }
 
     while (optind < argc) {
-        if (opts.nodereference == 1) {
+        if (opts.no_dereference == 1) {
             if (lchown(argv[optind], -1, grp_buf->gr_gid) != 0) {
-                perror("chown failed");
+                fprintf(stderr, "lchown failed on `%s'\n", argv[optind]);
             }
-
         }else {
             if (chown(argv[optind], -1, grp_buf->gr_gid) != 0) {
-                perror("chown failed");
+                fprintf(stderr, "chown failed on `%s'\n", argv[optind]);
             }
         }
 
         if (opts.verbose == 1) {
             printf("Changed group ownership of `%s' to `%s'\n", argv[optind], to_grp);
         }
-
         optind++;
     }
-
     return EXIT_SUCCESS;
 }
