2f2807261b5f64fd683db6ff9a3dd1b2c196cae5
Darren Kirby <darren@dragonbyte.ca>
2026-08-01 14:25:14 -0700

    Overhaul header/data output; implement --output/block-size

--- src/df.c
+++ src/df.c
@@ -27,20 +27,41 @@
 #include "mount.h"
 #include "common.h"
 
+/* Print all fields.
+ * 2047 = 0000011111111111 =
+ * source,fstype,itotal,iused,iavail,ipcent,size,used,avail,pcent,target */
+#define PRINT_ALL 2047
+/* Default fields to print.
+ * 1985 = 0000011111000001 =
+ * source,size,used,avail,pcent,target */
+#define PRINT_DEF 1985
+
 
 static const char *APP_NAME = "df";
 
+enum : uint16_t {
+    source = 1 << 0,
+    fstype = 1 << 1,
+    itotal = 1 << 2,
+    iused  = 1 << 3,
+    iavail = 1 << 4,
+    ipcent = 1 << 5,
+    size   = 1 << 6,
+    used   = 1 << 7,
+    avail  = 1 << 8,
+    pcent  = 1 << 9,
+    target = 1 << 10
+};
+
 static struct  {
+    uint16_t fields;
     bool total;
-    bool inodes;
-    bool fs_type;
     bool inc_dummy;
     bool sync;
     uint8_t format;
 } opts = {
+    .fields = PRINT_DEF,
     .total = false,
-    .inodes = false,
-    .fs_type = false,
     .inc_dummy = false,
     .sync = false,
     .format = 0 };
@@ -54,9 +75,13 @@ Options:\n\
     -i, --inodes\tdisplay information for inodes\n\
     -s, --sync\t\tsync all I/O before retrieving FS info\n\
     -T, --fs-type\tdisplay file type name\n\
-    -a, --all\t\tdisplay info for dummy file systems\n\
+    -a, --all\t\tdisplay info for dummy file systems\n\n\
     -[k|m|g] --[kilobytes|megabytes|gigabytes]\n\
-    \t\t\tdisplay in unit rather than blocks\n\
+        equivalent to -bK, -bM, -bG\n\n\
+    -o, --output[=field[,]...]\n\
+        Comma-delimited list of fields to print. With no arg all fields\n\
+        are printed. Possible fields are: 'source', 'fstype', 'itotal',\n\
+        'iused', 'iavail', 'ipcent', 'size', 'used', 'avail', 'pcent', and 'target'.\n\n\
 Report bugs to <darren@dragonbyte.ca>\n", APP_NAME);
 }
 
@@ -71,68 +96,143 @@ static uint64_t calculate_percent(const uint64_t total, const uint64_t free)
     return result;
 }
 
+typedef struct  {
+    const char* name;
+    uint16_t bit_pos;
+} FieldMap;
+
+static const FieldMap FIELD_MAPPINGS[] = {
+    {.name = "source", .bit_pos = 0}, {.name = "fstype", .bit_pos = 1},
+    {.name = "itotal", .bit_pos = 2}, {.name = "iused",  .bit_pos = 3},
+    {.name = "iavail", .bit_pos = 4}, {.name = "ipcent", .bit_pos = 5},
+    {.name = "size",   .bit_pos = 6}, {.name = "used",   .bit_pos = 7},
+    {.name = "avail",  .bit_pos = 8}, {.name = "pcent",  .bit_pos = 9},
+    {.name = "target", .bit_pos = 10}
+};
+
+static uint16_t parse_fields(char* arg)
+{
+    constexpr char delim[] = ",";
+    uint16_t r = 0;
+
+    char* token = strtok(arg, delim);
+    while (token != NULL) {
+        bool found = false;
+
+        for (size_t i = 0; i < sizeof(FIELD_MAPPINGS) / sizeof(FIELD_MAPPINGS[0]); ++i) {
+            if (strcmp(token, FIELD_MAPPINGS[i].name) == 0) {
+                r |= 1 << FIELD_MAPPINGS[i].bit_pos;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            fprintf(stderr, "%s: invalid field name: %s\n", APP_NAME, token);
+            exit(EXIT_FAILURE);
+        }
+
+        token = strtok(NULL, delim);
+    }
+    return r;
+}
+
 int main(const int argc, char *argv[])
 {
     const struct option longopts[] = {
-        { .name = "help",      .has_arg = no_argument, .flag = nullptr, .val = 'h' },
-        { .name = "version",   .has_arg = no_argument, .flag = nullptr, .val = 'V' },
-        { .name = "inodes",    .has_arg = no_argument, .flag = nullptr, .val = 'i' },
-        { .name = "kilobytes", .has_arg = no_argument, .flag = nullptr, .val = 'k' },
-        { .name = "megabytes", .has_arg = no_argument, .flag = nullptr, .val = 'm' },
-        { .name = "gigabytes", .has_arg = no_argument, .flag = nullptr, .val = 'g' },
-        { .name = "sync",      .has_arg = no_argument, .flag = nullptr, .val = 's' },
-        { .name = "fs-type",   .has_arg = no_argument, .flag = nullptr, .val = 'T' },
-        { .name = "total",     .has_arg = no_argument, .flag = nullptr, .val = 't' },
-        { .name = "all",       .has_arg = no_argument, .flag = nullptr, .val = 'a' },
-        { .name = nullptr,     .has_arg = no_argument, .flag = nullptr, .val = 0 }
+        { .name = "help",       .has_arg = no_argument,       .flag = nullptr, .val = 'h' },
+        { .name = "version",    .has_arg = no_argument,       .flag = nullptr, .val = 'V' },
+        { .name = "inodes",     .has_arg = no_argument,       .flag = nullptr, .val = 'i' },
+        { .name = "kilobytes",  .has_arg = no_argument,       .flag = nullptr, .val = 'k' },
+        { .name = "megabytes",  .has_arg = no_argument,       .flag = nullptr, .val = 'm' },
+        { .name = "gigabytes",  .has_arg = no_argument,       .flag = nullptr, .val = 'g' },
+        { .name = "sync",       .has_arg = no_argument,       .flag = nullptr, .val = 's' },
+        { .name = "fs-type",    .has_arg = no_argument,       .flag = nullptr, .val = 'T' },
+        { .name = "total",      .has_arg = no_argument,       .flag = nullptr, .val = 't' },
+        { .name = "all",        .has_arg = no_argument,       .flag = nullptr, .val = 'a' },
+        { .name = "output",     .has_arg = optional_argument, .flag = nullptr, .val = 'o' },
+        { .name = "block-size", .has_arg = required_argument, .flag = nullptr, .val = 'b' },
+        { .name = nullptr,      .has_arg = no_argument,       .flag = nullptr, .val = 0 }
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "VhikmgsTtd", longopts, nullptr)) != -1) {
+    while ((opt = getopt_long(argc, argv, "VhikmgsTtdo::b:", longopts, nullptr)) != -1) {
         switch(opt) {
-            case 'V':
-                printf("%s (%s) version %s\n", APP_NAME, APP_SUITE, APP_VERSION);
-                printf("%s compiled on %s at %s\n",
-                       strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__,
-                       __DATE__, __TIME__);
-                return EXIT_SUCCESS;
-                break;
-            case 'h':
-                show_help();
-                return EXIT_SUCCESS;
-                break;
-            case 'i':
-                opts.inodes = true;
-                break;
+        case 'V':
+            printf("%s (%s) version %s\n", APP_NAME, APP_SUITE, APP_VERSION);
+            printf("%s compiled on %s at %s\n",
+                   strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__,
+                   __DATE__, __TIME__);
+            return EXIT_SUCCESS;
+            break;
+        case 'h':
+            show_help();
+            return EXIT_SUCCESS;
+            break;
+        case 'i':
+            /* Zero-out bits 6-9. */
+            opts.fields &= ~(0b1111 << 6);
+            /* Set bits 2-5. */
+            opts.fields |= 0b1111 << 2;
+            break;
+        case 'k':
+            opts.format = 1;
+            break;
+        case 'm':
+            opts.format = 2;
+            break;
+        case 'g':
+            opts.format = 3;
+            break;
+        case 's':
+            opts.sync = true;
+            break;
+        case 'T':
+            /* Set bit 1. */
+            opts.fields |= 1 << 1;
+            break;
+        case 't':
+            opts.total = true;
+            break;
+        case 'd':
+            opts.inc_dummy = true;
+            break;
+        case 'o':
+            if (optarg) {
+                opts.fields = parse_fields(optarg);
+            } else {
+                /* No arg: print everything. */
+                opts.fields = PRINT_ALL;
+            }
+            break;
+        case 'b':
+            switch (optarg[0]) {
             case 'k':
+            case 'K':
                 opts.format = 1;
                 break;
             case 'm':
+            case 'M':
                 opts.format = 2;
                 break;
             case 'g':
+            case 'G':
                 opts.format = 3;
                 break;
-            case 's':
-                opts.sync = true;
-                break;
-            case 'T':
-                opts.fs_type = true;
-                break;
-            case 't':
-                opts.total = true;
-                break;
-            case 'd':
-                opts.inc_dummy = true;
-                break;
             default:
-                show_help();
-                EXIT_FAILURE;
-                break;
+                fprintf(stderr, "%s: invalid block size specifier: %s", APP_NAME, optarg);
+                return EXIT_FAILURE;
+            }
+            break;
+        default:
+            show_help();
+            EXIT_FAILURE;
+            break;
         }
     }
 
     int n_mounts = 0;
+    const int args = argc - optind;
     vfs_stat_t *mfs;
 
     if (argc == optind) {
@@ -155,9 +255,6 @@ int main(const int argc, char *argv[])
         }
     } else {
         /* Args passed... */
-        const int args = argc - optind;
-
-        /* Assuming n_mounts is reset to 0 before this block */
         mfs = malloc(sizeof(vfs_stat_t) * args);
         if (!mfs) {
             fprintf(stderr, "%s: malloc failed!\n", APP_NAME);
@@ -167,7 +264,7 @@ int main(const int argc, char *argv[])
         while (optind < argc) {
             vfs_stat_t tmp;
 
-            /* Calls native statfs on Mac, and your custom statfs_ext on Linux */
+            /* Calls native statfs on Mac, and custom statfs_ext on Linux */
             if (vfs_statfs(argv[optind], &tmp) == -1) {
                 fprintf(stderr, "%s: %s: %s\n", APP_NAME, argv[optind], strerror(errno));
                 optind++;
@@ -193,6 +290,9 @@ int main(const int argc, char *argv[])
             n_mounts++;
         }
     }
+    /* Reset optind. */
+    optind -= args;
+
     /* Determine column widths and math divisors upfront */
     int w = 12;
     uint64_t div = 1;
@@ -214,18 +314,52 @@ int main(const int argc, char *argv[])
         }
     }
 
-    /* Print the Header using the dynamic width 'w' */
-    printf("%-16s ", "Filesystem");
-    if (opts.fs_type) {
+    /* Print the selected headers. */
+    if (opts.fields & source) {
+        printf("%-16s ", "Filesystem");
+    }
+
+    if (opts.fields & fstype) {
         printf("%-7s ", "FS type");
     }
 
-    /* Use %-*s to pass the width variable 'w', followed by ONE space. */
-    printf("%-*s %-*s %-*s %4s   Mount point\n",
-           w, size_label,
-           w, "Used",
-           w, "Free",
-           "Use%");
+    if (opts.fields & itotal) {
+        printf("%-12s ", "Inodes");
+    }
+
+    if (opts.fields & iused) {
+        printf("%-8s ", "IUsed");
+    }
+
+    if (opts.fields & iavail) {
+        printf("%-12s ", "IFree");
+    }
+
+    if (opts.fields & ipcent) {
+        printf("%5s ", "IUse%");
+    }
+
+    if (opts.fields & size) {
+        printf("%-*s ", w, size_label);
+    }
+
+    if (opts.fields & used) {
+        printf("%-*s ", w, "Used");
+    }
+
+    if (opts.fields & avail) {
+        printf("%-*s ", w, "Free");
+    }
+
+    if (opts.fields & pcent) {
+        printf("%4s ", "Use%");
+    }
+
+    if (opts.fields & target) {
+        printf(" %s", "Mount Point");
+    }
+
+    putchar('\n');
 
     /* Print the Data */
     for (int i = 0; i < n_mounts; i++) {
@@ -251,21 +385,52 @@ int main(const int argc, char *argv[])
             p_used = used_bytes / div;
         }
 
-        const uint64_t pct = mfs[i].f_blocks == 0 ? 0 : calculate_percent(mfs[i].f_blocks, mfs[i].f_bfree);
+        /* Print the selected data. */
+        if (opts.fields & source) {
+            printf("%-16s ", mfs[i].f_mntfromname);
+        }
 
-        /* Print row headers. */
-        printf("%-16s ", mfs[i].f_mntfromname);
-        if (opts.fs_type) {
+        if (opts.fields & fstype) {
             printf("%-7s ", mfs[i].f_fstypename);
         }
 
-        /* Print sizes using the exact same 'w' dynamic width and ONE space. */
-        printf("%-*" PRId64 " %-*" PRId64 " %-*" PRId64 " %3" PRId64 "%%   %s\n",
-               w, p_size,
-               w, p_used,
-               w, p_free,
-               pct,
-               mfs[i].f_mntonname);
+        if (opts.fields & itotal) {
+            printf("%-12" PRId64 " ", mfs[i].f_files);
+        }
+
+        if (opts.fields & iused) {
+            printf("%-8" PRId64 " ", mfs[i].f_files - mfs[i].f_ffree);
+        }
+
+        if (opts.fields & iavail) {
+            printf("%-12" PRId64 " ", mfs[i].f_ffree);
+        }
+
+        if (opts.fields & ipcent) {
+            printf("%4" PRId64 "%% ", calculate_percent(mfs[i].f_files, mfs[i].f_ffree));
+        }
+
+        if (opts.fields & size) {
+            printf("%-*" PRId64 " ", w, p_size);
+        }
+
+        if (opts.fields & used) {
+            printf("%-*" PRId64 " ", w, p_used);
+        }
+
+        if (opts.fields & avail) {
+            printf("%-*" PRId64 " ", w, p_free);
+        }
+
+        if (opts.fields & pcent) {
+            printf("%3" PRId64 "%% ", calculate_percent(mfs[i].f_blocks, mfs[i].f_bfree));
+        }
+
+        if (opts.fields & target) {
+            printf(" %s", mfs[i].f_mntonname);
+        }
+
+        putchar('\n');
     }
 
     free(mfs);
